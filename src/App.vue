b52126d7dcde1f6256e77d1e1c9de3f61e3032f1
xiaojin22 <55696467+xiaojin22@users.noreply.github.com>
2019-11-27 12:55:52 +0800

    02,配置router模块，login页面配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <!-- //映射全部router组件 -->
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入路由模块router
+import router from './routers/index.js'
 Vue.config.productionTip = false
 
 new Vue({
+  // 注入
+  router,
   render: h => h(App)
 }).$mount('#app')
